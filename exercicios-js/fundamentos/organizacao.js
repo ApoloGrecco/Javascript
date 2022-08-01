79887e6a2affb1767b37255358e3879091a6d1be
ApoloGrecco <apolo.argolo@gmail.com>
2022-08-01 14:16:43 -0300

    4 - Fundamentos - Parte 1

--- exercicios-js/fundamentos/organizacao.js
+++ exercicios-js/fundamentos/organizacao.js
@@ -3,6 +3,6 @@ console.log("Sentença de código")
 {
     {
         console.log("Ola");
-        console.log("Mundo!") // Padrão que irei uso no decorrer no código!
+        console.log("Mundo!") // Padrão que irei uso no decorrer dos estudos!
     }
 }
