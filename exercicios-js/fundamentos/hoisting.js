0ade74d91535d1c9b6a5c93d60e0c8c1a4d68abe
ApoloGrecco <apolo.argolo@gmail.com>
2022-08-11 14:05:27 -0300

    4 - Fundamentos - Parte 8

---
+++ exercicios-js/fundamentos/hoisting.js
@@ -0,0 +1,7 @@
+console.log("a = ", a)
+var a = 2
+console.log("a = ", a)
+
+console.log("b = ", b) // Gera erro pois o valor de B não foi declarado antes.
+let b = 2
+console.log("b = ", b)
---
+++ exercicios-js/fundamentos/usandoLetEmLoop.js
@@ -0,0 +1,5 @@
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
+
+console.log('i =', i) // Gera erro, pois o valor de I só é visível dentro do bloco do for.
